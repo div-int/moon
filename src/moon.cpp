6160b595b381e91cff94fb7f2b92575f5d236ac9
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-11-17 23:46:00 +0000

    Test out how the graphics chip may work

--- src/moon.cpp
+++ src/moon.cpp
@@ -3,9 +3,289 @@
 
 #include "moon.h"
 
+Moon::Moon()
+{
+	sAppName = "Moon";
+}
+
+Moon::~Moon()
+{
+	// TO DO
+}
+
+bool Moon::OnUserCreate()
+{
+	// TO DO
+
+	pixel_x = 0;
+	pixel_y = 0;
+
+	display_scale = 1;
+	display_width = ScreenWidth() / display_scale;
+	display_height = ScreenHeight() / display_scale;
+	display_size = display_width * display_height;
+
+	display_buffer = new olc::Sprite(display_width, display_height);
+
+	screen_buffer_0 = new uint8_t[1024 * 1024];
+	screen_buffer_1 = new uint8_t[1024 * 1024];
+	screen_buffer_2 = new uint8_t[1024 * 1024];
+	screen_buffer_3 = new uint8_t[1024 * 1024];
+
+	for (uint32_t i = 0; i < (1024 * 1024); i++)
+	{
+		screen_buffer_0[i] = (((i / (32 * 1024)) + (i / 32)) % 2) * rand();
+		screen_buffer_1[i] = (((i / (16 * 1024)) + (i / 16)) % 2) * rand();
+		screen_buffer_2[i] = (((i / (8 * 1024)) + (i / 8)) % 2) * rand();
+		screen_buffer_3[i] = (((i / (4 * 1024)) + (i / 4)) % 2) * rand();
+	}
+
+	pixel_x_start_0 = 0;
+	pixel_y_start_0 = 0;
+	pixel_x_start_1 = 0;
+	pixel_y_start_1 = 0;
+	pixel_x_start_2 = 0;
+	pixel_y_start_2 = 0;
+	pixel_x_start_3 = 0;
+	pixel_y_start_3 = 0;
+
+	pixel_x_scale_0 = 0x10000;
+	pixel_y_scale_0 = 0x10000;
+	pixel_x_scale_1 = 0x10000;
+	pixel_y_scale_1 = 0x10000;
+	pixel_x_scale_2 = 0x10000;
+	pixel_y_scale_2 = 0x10000;
+	pixel_x_scale_3 = 0x10000;
+	pixel_y_scale_3 = 0x10000;
+
+	screen_buffer_enabled = 0b1111;
+
+	return true;
+}
+
+bool Moon::OnUserDestroy()
+{
+	// TO DO
+
+	return true;
+}
+
+bool Moon::OnUserUpdate(float fElapsedTime)
+{
+	// TO DO
+
+	SetDrawTarget(display_buffer);
+
+	// Update pixel location
+
+	pixel_x = 0;
+	pixel_y = 0;
+
+	pixel_x_start_0 = (-GetMouseX() << 15) & 0x3ffffff;
+	pixel_y_start_0 = (-GetMouseY() << 15) & 0x3ffffff;
+
+	pixel_x_start_1 = (-GetMouseX() << 14) & 0x3ffffff;
+	pixel_y_start_1 = (-GetMouseY() << 14) & 0x3ffffff;
+
+	pixel_x_start_2 = (-GetMouseX() << 13) & 0x3ffffff;
+	pixel_y_start_2 = (-GetMouseY() << 13) & 0x3ffffff;
+
+	pixel_x_start_3 = (-GetMouseX() << 12) & 0x3ffffff;
+	pixel_y_start_3 = (-GetMouseY() << 12) & 0x3ffffff;
+
+	pixel_x_0 = pixel_x_start_0 & 0x3ffffff;
+	pixel_y_0 = pixel_y_start_0 & 0x3ffffff;
+
+	pixel_x_1 = pixel_x_start_1 & 0x3ffffff;
+	pixel_y_1 = pixel_y_start_1 & 0x3ffffff;
+
+	pixel_x_2 = pixel_x_start_2 & 0x3ffffff;
+	pixel_y_2 = pixel_y_start_2 & 0x3ffffff;
+
+	pixel_x_3 = pixel_x_start_3 & 0x3ffffff;
+	pixel_y_3 = pixel_y_start_3 & 0x3ffffff;
+
+	if (GetKey(olc::Key::ESCAPE).bReleased)
+		return false;
+
+	if (GetKey(olc::Key::K1).bPressed)
+		screen_buffer_enabled ^= 0b0001;
+	if (GetKey(olc::Key::K2).bPressed)
+		screen_buffer_enabled ^= 0b0010;
+	if (GetKey(olc::Key::K3).bPressed)
+		screen_buffer_enabled ^= 0b0100;
+	if (GetKey(olc::Key::K4).bPressed)
+		screen_buffer_enabled ^= 0b1000;
+
+	if (GetKey(olc::Key::RIGHT).bHeld)
+	{
+		pixel_x_scale_0 += 0x100;
+		pixel_x_scale_1 += 0x100;
+		pixel_x_scale_2 += 0x100;
+		pixel_x_scale_3 += 0x100;
+	}
+	if (GetKey(olc::Key::LEFT).bHeld)
+	{
+		pixel_x_scale_0 -= 0x100;
+		pixel_x_scale_1 -= 0x100;
+		pixel_x_scale_2 -= 0x100;
+		pixel_x_scale_3 -= 0x100;
+	}
+	if (GetKey(olc::Key::UP).bHeld)
+	{
+		pixel_y_scale_0 += 0x100;
+		pixel_y_scale_1 += 0x100;
+		pixel_y_scale_2 += 0x100;
+		pixel_y_scale_3 += 0x100;
+	}
+	if (GetKey(olc::Key::DOWN).bHeld)
+	{
+		pixel_y_scale_0 -= 0x100;
+		pixel_y_scale_1 -= 0x100;
+		pixel_y_scale_2 -= 0x100;
+		pixel_y_scale_3 -= 0x100;
+	}
+
+	auto pixel_colour = olc::VERY_DARK_GREY;
+	uint32_t offset;
+	uint32_t pixel_lookup;
+
+	for (int i = 0; i < display_size; i++)
+	{
+		pixel_colour = olc::VERY_DARK_YELLOW;
+
+		if (screen_buffer_enabled & 0b0001)
+		{
+			offset = (pixel_x_0 >> 16) + ((pixel_y_0 >> 16) << 10);
+			pixel_lookup = screen_buffer_0[offset];
+		}
+		else
+		{
+			pixel_lookup = 0x00;
+		}
+
+		if (pixel_lookup != 0x00)
+		{
+			pixel_colour.r = pixel_lookup;
+			pixel_colour.g = 0x00;
+			pixel_colour.b = 0x00;
+		}
+		else
+		{
+			if (screen_buffer_enabled & 0b0010)
+			{
+				offset = (pixel_x_1 >> 16) + ((pixel_y_1 >> 16) << 10);
+				pixel_lookup = screen_buffer_1[offset];
+			}
+			else
+			{
+				pixel_lookup = 0x00;
+			}
+
+			if (pixel_lookup != 0x00)
+			{
+				pixel_colour.r = 0x00;
+				pixel_colour.g = pixel_lookup;
+				pixel_colour.b = 0x00;
+			}
+			else
+			{
+				if (screen_buffer_enabled & 0b0100)
+				{
+					offset = (pixel_x_2 >> 16) + ((pixel_y_2 >> 16) << 10);
+					pixel_lookup = screen_buffer_2[offset];
+				}
+				else
+				{
+					pixel_lookup = 0x00;
+				}
+
+				if (pixel_lookup != 0x00)
+				{
+					pixel_colour.r = 0x00;
+					pixel_colour.g = 0x00;
+					pixel_colour.b = pixel_lookup;
+				}
+				else
+				{
+					if (screen_buffer_enabled & 0b1000)
+					{
+						offset = (pixel_x_3 >> 16) + ((pixel_y_3 >> 16) << 10);
+						pixel_lookup = screen_buffer_3[offset];
+					}
+					else
+					{
+						pixel_lookup = 0x00;
+					}
+
+					if (pixel_lookup != 0x00)
+					{
+						pixel_colour.r = pixel_lookup;
+						pixel_colour.g = pixel_lookup;
+						pixel_colour.b = pixel_lookup;
+					}
+				}
+			}
+		}
+
+		Draw(pixel_x, pixel_y, pixel_colour);
+
+		++pixel_x;
+
+		if (pixel_x >= display_width)
+		{
+			pixel_x = 0;
+
+			++pixel_y;
+
+			pixel_x_0 = pixel_x_start_0;
+			pixel_x_1 = pixel_x_start_1;
+			pixel_x_2 = pixel_x_start_2;
+			pixel_x_3 = pixel_x_start_3;
+
+			pixel_y_0 += pixel_y_scale_0;
+			pixel_y_1 += pixel_y_scale_1;
+			pixel_y_2 += pixel_y_scale_2;
+			pixel_y_3 += pixel_y_scale_3;
+		}
+
+		pixel_x_0 += pixel_x_scale_0;
+		pixel_x_1 += pixel_x_scale_1;
+		pixel_x_2 += pixel_x_scale_2;
+		pixel_x_3 += pixel_x_scale_3;
+
+		pixel_x_0 &= 0x3ffffff;
+		pixel_y_0 &= 0x3ffffff;
+		pixel_x_1 &= 0x3ffffff;
+		pixel_y_1 &= 0x3ffffff;
+		pixel_x_2 &= 0x3ffffff;
+		pixel_y_2 &= 0x3ffffff;
+		pixel_x_3 &= 0x3ffffff;
+		pixel_y_3 &= 0x3ffffff;
+	}
+
+	pixel_x_start_0 += pixel_x_scale_0;
+	pixel_x_start_1 -= pixel_x_scale_1;
+	pixel_y_start_2 += pixel_y_scale_2;
+	pixel_y_start_3 -= pixel_y_scale_3;
+
+	pixel_y_start_0 += pixel_x_scale_0 >> 1;
+	pixel_y_start_1 -= pixel_x_scale_1 >> 1;
+	pixel_x_start_2 += pixel_y_scale_2 >> 1;
+	pixel_x_start_3 -= pixel_y_scale_3 >> 1;
+
+	SetDrawTarget(nullptr);
+	DrawSprite(0, 0, display_buffer, display_scale);
+
+	return true;
+}
+
 int main()
 {
-	std::cout << "Hello Moon!" << std::endl;
-	
+	Moon moon;
+
+	if (moon.Construct(848, 480, 2, 2, true, true))
+		moon.Start();
+
 	return OK;
 }
--- src/moon.h
+++ src/moon.h
@@ -5,7 +5,66 @@
 
 #include <iostream>
 
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
 const int OK = 0;
 const int FAIL = -1;
 
-// TODO: Reference additional headers your program requires here.
+class Moon : public olc::PixelGameEngine
+{
+private:
+	uint32_t pixel_x;
+	uint32_t pixel_y;
+
+	uint32_t display_scale;
+	uint32_t display_width;
+	uint32_t display_height;
+	uint32_t display_size;
+
+	olc::Sprite* display_buffer;
+
+	uint32_t pixel_x_0;
+	uint32_t pixel_y_0;
+	uint32_t pixel_x_1;
+	uint32_t pixel_y_1;
+	uint32_t pixel_x_2;
+	uint32_t pixel_y_2;
+	uint32_t pixel_x_3;
+	uint32_t pixel_y_3;
+
+	uint32_t pixel_x_start_0;
+	uint32_t pixel_y_start_0;
+	uint32_t pixel_x_start_1;
+	uint32_t pixel_y_start_1;
+	uint32_t pixel_x_start_2;
+	uint32_t pixel_y_start_2;
+	uint32_t pixel_x_start_3;
+	uint32_t pixel_y_start_3;
+
+	uint32_t pixel_x_scale_0;
+	uint32_t pixel_y_scale_0;
+	uint32_t pixel_x_scale_1;
+	uint32_t pixel_y_scale_1;
+	uint32_t pixel_x_scale_2;
+	uint32_t pixel_y_scale_2;
+	uint32_t pixel_x_scale_3;
+	uint32_t pixel_y_scale_3;
+
+	uint8_t *screen_buffer_0;
+	uint8_t *screen_buffer_1;
+	uint8_t *screen_buffer_2;
+	uint8_t *screen_buffer_3;
+
+	uint8_t screen_buffer_enabled;
+
+protected:
+
+public:
+	Moon();
+	~Moon();
+
+	bool OnUserCreate() override;
+	bool Moon::OnUserDestroy() override;
+	bool OnUserUpdate(float fElapsedTime) override;
+};
